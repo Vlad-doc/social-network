eac5c45d0c580be2c5e58e04d1a9ca428ab5d798
Your Name <you@example.com>
2022-06-30 14:20:04 +0300

    page dialoga wth NavLink

--- src/components/dialogs/dialogs.js
+++ src/components/dialogs/dialogs.js
@@ -1,8 +1,34 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 import dialogsStyle from "./dialogs.module.css"
 
 function Dialogs() {
-  return <div className={dialogsStyle}>Dialogs</div>
+  return (
+    <div className={dialogsStyle.dialogs}>
+      <div className={dialogsStyle.dialogItems}>
+        <div className={dialogsStyle.dialog}>
+          <NavLink to="/dialogs/1">Valera</NavLink>
+        </div>
+        <div className={dialogsStyle.dialog}>
+          <NavLink to="/dialogs/2">Valera</NavLink>
+        </div>
+        <div className={dialogsStyle.dialog}>
+          <NavLink to="/dialogs/3">Valera</NavLink>
+        </div>
+        <div className={dialogsStyle.dialog}>
+          <NavLink to="/dialogs/4">Valera</NavLink>
+        </div>
+        <div className={dialogsStyle.dialog}>
+          <NavLink to="/dialogs/5">Valera</NavLink>
+        </div>
+      </div>
+      <div className={dialogsStyle.messages}>
+        <div className={dialogsStyle.message}>Hello</div>
+        <div className={dialogsStyle.message}>Hi</div>
+        <div className={dialogsStyle.message}>Privet</div>
+      </div>
+    </div>
+  )
 }
 
 export default Dialogs
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,22 +3,34 @@ import { NavLink } from "react-router-dom"
 import navStyle from "./navbar.module.css"
 
 function Navbar() {
+  let activeClass = ({ isActive }) =>
+    isActive ? `${navStyle.active}` : `${navStyle.item}`
   return (
     <nav className={navStyle.nav}>
       <div className={navStyle.item}>
-        <NavLink to="/profile">Profile</NavLink>
+        <NavLink to="/profile" className={activeClass}>
+          Profile
+        </NavLink>
       </div>
-      <div className={`${navStyle.item} ${navStyle.active}`}>
-        <NavLink to="/dialogs">Messages</NavLink>
+      <div className={navStyle.item}>
+        <NavLink to="/dialogs" className={activeClass}>
+          Messages
+        </NavLink>
       </div>
-      <div className="item">
-        <NavLink to="/news">News</NavLink>
+      <div className={navStyle.item}>
+        <NavLink to="/news" className={activeClass}>
+          News
+        </NavLink>
       </div>
-      <div className="item">
-        <NavLink to="/music">Music</NavLink>
+      <div className={navStyle.item}>
+        <NavLink to="/music" className={activeClass}>
+          Music
+        </NavLink>
       </div>
-      <div className="item">
-        <a href="/settings">Settings</a>
+      <div className={navStyle.item}>
+        <NavLink to="/" className={activeClass}>
+          Settings
+        </NavLink>
       </div>
     </nav>
   )
