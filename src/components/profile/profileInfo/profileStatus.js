16085255d2e834b7b152c58c07bb7cee16c50ba6
Your Name <you@example.com>
2022-08-04 17:17:55 +0300

    handleChange

--- src/components/profile/profileInfo/profileStatus.js
+++ src/components/profile/profileInfo/profileStatus.js
@@ -4,23 +4,21 @@ import profInfoStyle from "./profileInfo.module.css"
 
 const ProfileStatus = (props) => {
   const [status, setStatus] = useState(true)
-  const activateEditMode = () => {
+  const handleChange = () => {
     setStatus((prevState) => !prevState)
   }
-  const deactivateEditMode = () => {
-    setStatus((prevState) => !prevState)
-  }
+
   return (
     <div className={profInfoStyle.status}>
       {status ? (
-        <h4 onDoubleClick={activateEditMode}>{props.status}</h4>
+        <h4 onDoubleClick={handleChange}>{props.status}</h4>
       ) : (
         <div>
           <input
             type="text"
             autoFocus={true}
             value={props.status}
-            onBlur={deactivateEditMode}
+            onBlur={handleChange}
             onChange={() => {}}
           />
         </div>
