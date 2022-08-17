726ce7b7bebf399909b4285314f4416a86f0b3f7
Vladislav <vk@gmail.com>
2022-08-17 23:12:39 +0300

    login field password

--- src/components/common/formsControls/formsControls.js
+++ src/components/common/formsControls/formsControls.js
@@ -11,12 +11,13 @@ export const TextArea = ({ input, meta: { touched, error } }) => {
     </div>
   )
 }
-export const LoginControl = ({ input, meta: { touched, error } }) => {
+export const LoginControl = ({ input, meta: { touched, error }, ...props }) => {
   return (
     <div className={style.input}>
       <input
         {...input}
         className={!touched ? undefined : error ? style.error : undefined}
+        {...props}
       />
       <h3>{touched && error && <span>{error}</span>}</h3>
     </div>
