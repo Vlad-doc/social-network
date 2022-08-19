19d3ceb2f4b4a765e939ad298c399f1d8a4c721b
Vladislav <vk@gmail.com>
2022-08-20 02:28:54 +0300

    design nav && content

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,6 +3,12 @@ import { NavLink } from "react-router-dom"
 import { Friends } from "./friends/friends"
 import navStyle from "./navbar.module.css"
 import { connect } from "react-redux/es/exports"
+import profIcon from "../../assets/images/gg_profile.png"
+import newsIcon from "../../assets/images/bx_news.png"
+import messageIcon from "../../assets/images/ci_message-circle.png"
+import usersIcon from "../../assets/images/bi_people.png"
+import musicIcon from "../../assets/images/eva_music-outline.png"
+import settingsIcon from "../../assets/images/eva_settings-outline.png"
 
 function Navbar(props) {
   let activeClass = ({ isActive }) =>
@@ -11,35 +17,47 @@ function Navbar(props) {
   return (
     <nav className={navStyle.nav}>
       <div className={navStyle.item}>
+        <img src={profIcon} alt="profile" />
         <NavLink to="/profile" className={activeClass}>
           Profile
         </NavLink>
       </div>
+
       <div className={navStyle.item}>
-        <NavLink to="/dialogs" className={activeClass}>
-          Messages
-        </NavLink>
-      </div>
-      <div className={navStyle.item}>
-        <NavLink to="/users" className={activeClass}>
-          Users
-        </NavLink>
-      </div>
-      <div className={navStyle.item}>
+        <img src={newsIcon} alt="news" />
         <NavLink to="/news" className={activeClass}>
           News
         </NavLink>
       </div>
+
       <div className={navStyle.item}>
+        <img src={messageIcon} alt="message" />
+        <NavLink to="/dialogs" className={activeClass}>
+          Messages
+        </NavLink>
+      </div>
+
+      <div className={navStyle.item}>
+        <img src={usersIcon} alt="users" />
+        <NavLink to="/users" className={activeClass}>
+          Users
+        </NavLink>
+      </div>
+
+      <div className={navStyle.item}>
+        <img src={musicIcon} alt="profile" />
         <NavLink to="/music" className={activeClass}>
           Music
         </NavLink>
       </div>
+
       <div className={navStyle.item}>
+        <img src={settingsIcon} alt="settings" />
         <NavLink to="/settings" className={activeClass}>
           Settings
         </NavLink>
       </div>
+
       <h4>Friends</h4>
       <div className={navStyle.item__friends}>
         {props.friendsPage.friends.map((friend) => (
--- src/components/profile/profileInfo/profileInfo.js
+++ src/components/profile/profileInfo/profileInfo.js
@@ -2,32 +2,52 @@ import React from "react"
 import profInfoStyle from "./profileInfo.module.css"
 import notFoundPNG from "../../../assets/images/not_found.png"
 import ProfileStatus from "./profileStatus"
+import { Avatar, Image } from "antd"
 
 export const ProfileInfo = (props) => {
+  console.log(props)
   return (
-    <div>
+    <>
       {props.profile ? (
         <>
-          <img
-            src={
-              props.profile.photos.large
-                ? props.profile.photos.large
-                : notFoundPNG
-            }
-            alt=""
-            width={"200px"}
-          />
-          <div className={profInfoStyle.description}>
-            {props.profile.fullName}
-            <ProfileStatus
-              status={props.status}
-              updateUserStatus={props.updateUserStatus}
+          <div className={profInfoStyle.bcImg}> </div>
+          <div className={profInfoStyle.content}>
+            {/* <img
+              src={
+                props.profile.photos.large
+                  ? props.profile.photos.large
+                  : notFoundPNG
+              }
+              alt=""
+              width={"200px"}
+            /> */}
+            <Avatar
+              src={
+                <Image
+                  src={
+                    props.profile.photos.large
+                      ? props.profile.photos.large
+                      : notFoundPNG
+                  }
+                  style={{ width: "100%", height: "100%" }}
+                />
+              }
+              size={170}
+              style={{ backgroundColor: "#f5f5f5" }}
             />
+            <div className={profInfoStyle.description}>
+              <div className={profInfoStyle.name}>{props.profile.fullName}</div>
+              <div className={profInfoStyle.about}>{props.profile.aboutMe}</div>
+              <ProfileStatus
+                status={props.status}
+                updateUserStatus={props.updateUserStatus}
+              />
+            </div>
           </div>
         </>
       ) : (
         props.profile
       )}
-    </div>
+    </>
   )
 }
