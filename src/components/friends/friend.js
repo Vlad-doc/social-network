64abbca2a428c5afb1b5d6aaad91fccc80d9de3f
Vladislav <vk@gmail.com>
2022-08-21 14:31:49 +0300

    design block friends

--- src/components/friends/friend.js
+++ src/components/friends/friend.js
@@ -1,11 +1,17 @@
 import React from "react"
 import style from "./friends.module.css"
+import message from "../../assets/images/messIcon.png"
 
 export const Friend = ({ friend }) => {
   return (
     <div className={style.item__friends_friend}>
-      <img src={friend.avatar} alt="ava" />
-      <div>{friend.name}</div>
+      <div className={style.info}>
+        <img src={friend.avatar} alt="ava" />
+        <div className={style.item__friends_friend_name}>{friend.name}</div>
+      </div>
+      <div className={style.mesBlock}>
+        <img src={message} alt="message" />
+      </div>
     </div>
   )
 }
